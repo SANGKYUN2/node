102ae026f035398a75567f77d31255ed9f766338
SANGKYUN2 <gr001231@naver.com>
2024-04-23 17:57:21 +0900

    ver02

--- ex02/app.js
+++ ex02/app.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/book', require('./routes/book'));
+app.use('/movie', require('./routes/movie'));
+app.use('/local', require('./routes/local'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- ex02/routes/movie.js
+++ ex02/routes/movie.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* 도서검색 */
 router.get('/', function(req, res, next) {
-  res.render('index.ejs', { title: '회사소개', pageName : 'home.ejs' });
+  res.render('index.ejs', { title: '영화검색', pageName : 'movie/search.ejs'});
 });
 
 module.exports = router;
+
